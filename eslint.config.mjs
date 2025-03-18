462dcbaefcfa09ef402cd84244049c95931d60a8
Devyd Silva <devyd.santana20201@gmail.com>
2025-03-18 15:56:40 -0300

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import pluginReact from 'eslint-plugin-react'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-
   {
     extends: [
       'next/core-web-vitals',
@@ -28,4 +28,6 @@ export default [
       '@typescript-eslint/explicit-module-boundary-types': 'off'
     }
   }
-]
+
+
+];
